db9eabf9d8599ec1b7452dceee1d26969bc553b1
arseniysychev <arseniysychev@gmail.com>
2016-04-18 13:57:51 +0300

    library dev

--- aec/apps/library/models.py
+++ aec/apps/library/models.py
@@ -2,6 +2,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+LEVELS_CHOICES = (
+    (0, 'BLES level'),
+    (1, 'GRAS level'),
+    (2, 'GRAS level'),
+    (3, 'GRAS level'),
+    (4, 'GRAS level'),
+    (5, 'GRAS level'),
+    (6, 'GRAS level'),
+    (7, 'HIGH level'),
+)
+
 
 class Library(models.Model):
-    title = models.CharField(max_length=50)
+    level = models.PositiveSmallIntegerField(choices=LEVELS_CHOICES,
+                                             blank=True, null=True)
+    lesson = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ("level", "lesson")
--- aec/apps/library/views.py
+++ aec/apps/library/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import LibrarySerializer
+from .models import Library
+
+
+class LibraryViewSet(viewsets.ViewSet):
+    queryset = Library.objects.all()
+
+    def list(self, request):
+        serializer = LibrarySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        lib = LibrarySerializer(data=request.data)
+        if lib.is_valid():
+            lib.save()
+            return Response(lib.data, status=201)
+        return Response(lib.errors, status=400)
