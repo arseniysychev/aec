50e45dcedbe38d0f462cdc9ca36ae1bc647157d5
arseniysychev <arseniysychev@gmail.com>
2016-04-25 12:18:17 +0300

    remove enchant

--- aec/apps/vocabulary/models.py
+++ aec/apps/vocabulary/models.py
@@ -2,14 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from aec.validators import is_english
+# from aec.validators import is_english
 from aec.apps.library.models import Library
 from .managers import CustomManager
 
 
 class Word(models.Model):
-    english = models.CharField(max_length=50, unique=True,
-                               validators=[is_english, ])
+    english = models.CharField(max_length=50, unique=True)
     translate = models.CharField(max_length=100)
     antonym = models.ManyToManyField("self", blank=True)
     library = models.ManyToManyField(Library, blank=True)
--- aec/validators.py
+++ aec/validators.py
@@ -1,19 +1,19 @@
-import enchant
-from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
-
-en_dict_us = enchant.Dict("en_US")
-
-
-def is_english(value):
-    for item in value.split():
-        if item and not en_dict_us.check(item):
-            raise ValidationError(
-                _('%(value)s is not in English dictionary. '
-                  'Please correct %(word)s: %(suggest)s'),
-                params={
-                    'value': value,
-                    'word': item,
-                    'suggest': en_dict_us.suggest(item),
-                },
-            )
+# import enchant
+# from django.core.exceptions import ValidationError
+# from django.utils.translation import ugettext_lazy as _
+#
+# en_dict_us = enchant.Dict("en_US")
+#
+#
+# def is_english(value):
+#     for item in value.split():
+#         if item and not en_dict_us.check(item):
+#             raise ValidationError(
+#                 _('%(value)s is not in English dictionary. '
+#                   'Please correct %(word)s: %(suggest)s'),
+#                 params={
+#                     'value': value,
+#                     'word': item,
+#                     'suggest': en_dict_us.suggest(item),
+#                 },
+#             )
