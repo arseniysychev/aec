b515e4a5c704c132f3643ecf5b6dcec716328488
arseniysychev <arseniysychev@gmail.com>
2016-05-27 09:54:05 +0300

    request load_dicts with GET param

--- aec/views.py
+++ aec/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.core.management import call_command
 
@@ -7,7 +8,15 @@ def index(request):
 
 
 def load_dicts(request):
-    for l in range(6):
-        less = l + 1
-        call_command('load_dict', file='1-{less}.csv'.format(less=less), level=1, lesson=less)
-    return redirect('index')
+    level = request.GET.get('level')
+    lesson = request.GET.get('lesson')
+    if level and lesson:
+        call_command(
+            'load_dict',
+            file='{level}-{lesson}.csv'.format(lesson=lesson, level=level),
+            level=level,
+            lesson=lesson
+        )
+        return redirect('index')
+    else:
+        return HttpResponse('No GET params "lesson" or "level"', status=400)
